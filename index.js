a2cdbb92ae9a693e8bd1efb827d4f7dc26018ae3
Saswata <saswatamaitra3.14@gmail.com>
2023-06-27 00:29:14 +0530

    Filter function

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ console.log(outputBinary);
 
 // Others way to write map function
 console.log("Others way to write map function");
-console.log("Defining whole fuction in the map function");
+console.log("Defining whole function in the map function");
 
 const double_example2 = arr.map(function double(x){return x * 2} )
 
@@ -41,3 +41,34 @@ const binary_example2 = arr.map(function binary(x){return x.toString(2)} )
 console.log(double_example2);
 console.log(tripple_example2);
 console.log(binary_example2);
+
+
+
+
+
+
+console.log("Using arrow function in the map function");
+
+const double_example3 = arr.map((x)=> x * 2 )
+
+const tripple_example3 = arr.map((x) => x * 3 )
+
+const binary_example3 = arr.map((x)=> x.toString(2) )
+
+console.log(double_example3);
+console.log(tripple_example3);
+console.log(binary_example3);
+
+
+// Filter Function
+console.log("Filter function");
+function isOdd(x){
+        return x % 2;
+}
+
+function isEven(x){
+    return x % 2 === 0;
+}
+
+console.log("All odd elements",arr.filter(isOdd)); 
+console.log("All Even elements",arr.filter(isEven));
